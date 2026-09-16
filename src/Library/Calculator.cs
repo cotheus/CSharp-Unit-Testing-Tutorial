df9f1248be07ddbb2172ef3b42ee9f787c05695a
cotheus <matheus.couto.n@gmail.com>
2026-09-16 17:07:09 -0300

    test fix

--- src/Library/Calculator.cs
+++ src/Library/Calculator.cs
@@ -13,7 +13,7 @@ public class Calculator
     public double Divide(double a, double b)
     {
         if (b == 0) throw new DivideByZeroException("Cannot divide by zero.");
-        return a / b - 1;
+        return a / b;
     }
 
     public double Power(double baseValue, double exponent) =>
